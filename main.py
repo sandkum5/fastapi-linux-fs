1a37cf74fe21f0105f598a7c6ef284e9f8e6c795
sandkum5 <sandeepkumar.er21@gmail.com>
2021-09-25 13:17:22 -0700

    Update file

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+#!/usr/bin/env python3
+
 from fastapi import FastAPI
 import os
 from pathlib import Path
 
 app = FastAPI()
 
-# This function
+#
+# This function returns the directories in root directory.
+#
 @app.get("/")  # Path Operation Decorator
 def read_root():  # Path operation Function
-    # return {"Hello": "World"}
+    # We are creating a data_dict. We are clearly stating its a dict.
     data_dict: dict = {}
     path = Path("/")
-    data = os.listdir(path)
-    data_dict[path] = data
+    # data = os.listdir(path)
+    data_dict[path] = os.listdir(path)
     return data_dict
 
 
+#
 # This function list directory files or file contents based on the specified path.
 # E.g.
-# If the path is a director, it will return the directory files.
+# If the path is a directory, it will return the directory files.
 # If the path is a file, it will return the contents of the file.
+#
 @app.get("/{file_path:path}")
 def get_path(file_path):
     path = Path(f"/{file_path}")
@@ -26,11 +32,11 @@ def get_path(file_path):
     if path.exists():
         if path.is_file():
             with open(path) as f:
-                data = f.read()
-                data_dict[path] = data
+                # data = f.read()
+                data_dict[path] = f.read()
         elif path.is_dir():
-            data = os.listdir(path)
-            data_dict[path] = data
+            # data = os.listdir(path)
+            data_dict[path] = os.listdir(path)
         return data_dict
     else:
         return "Path doesn't Exist"
